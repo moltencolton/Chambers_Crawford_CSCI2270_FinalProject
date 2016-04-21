34481cb5202396702e91ee2de9c7f4114c104056
moltencolton <colton.chambers@colorado.edu>
2016-04-20 21:05:00 -0600

    basic functions for quotetree

--- QuoteTree.cpp
+++ QuoteTree.cpp
@@ -1,5 +1,7 @@
 /* This file contains the class method implementations for QuoteTree */
-
+#include "QuoteTree.h"
+#include <iostream>
+using namespace std;
 QuoteTree::QuoteTree()
 {
     root = NULL;
@@ -8,4 +10,85 @@ QuoteTree::QuoteTree()
 QuoteTree::~QuoteTree()
 {
     DeleteAll(root);
+}
+int QuoteTree::countQuoteNodes()
+{
+    return quoteCount;
+}
+void QuoteTree::addQuoteNode(string quote, string author)
+{
+    quoteCount++;
+    QuoteNode *tmp = new QuoteNode;
+    tmp = root;
+    QuoteNode *node = new QuoteNode;
+    node -> quote = quote;
+    node -> author = author;
+    node -> right = NULL;
+    node -> left = NULL;
+    node -> parent = NULL;
+    QuoteNode *parent = new QuoteNode;
+    if(tmp == NULL){
+        root = node;
+        quoteCount=1;
+    }
+    else{
+    while(tmp!=NULL)
+    {
+        parent = tmp;
+        string quote1;
+        string quote2;
+        quote1 = node -> quote;
+        quote2 = tmp -> quote;
+        if(quote1.compare(quote2) < 0)
+        {
+            tmp = tmp->left;
+        }
+        else
+            tmp = tmp->right;
+    }
+    if(parent == NULL)
+    {
+        root = NULL;
+    }
+    else if(node -> quote.compare(parent-> quote) < 0)
+    {
+        parent -> left = node;
+        node -> parent = parent;
+    }
+    else
+    {
+        parent -> right = node;
+        node -> parent = parent;
+    }
+    }
+}
+void QuoteTree::findQuote(string quote, string author)
+{
+    bool found = false;
+    QuoteNode *node = root;
+    while(node!=NULL && !found)
+    {
+        if(node->quote.compare(quote)>0)
+            node = node -> left;
+        else if(node->quote.compare(quote)<0)
+            node = node -> right;
+        else
+        {
+            if(node->author == author)
+                cout<<"Quote is real;"<<endl;
+            else
+                cout<<"Quote is real but the real author is "<<node->author<<endl;
+            found = true;
+        }
+    }
+    if(!found)
+        cout << "Quote not found." << endl;
+}
+void QuoteTree::DeleteAll(QuoteNode* node)
+{
+    if(node -> left!= NULL)
+        DeleteAll(node->left);
+    if(node->right!=NULL)
+        DeleteAll(node->right);
+    delete node;
 }
--- QuoteTree.h
+++ QuoteTree.h
@@ -1,1 +1,46 @@
 /* This file contains the class definition for QuoteTree */
+#ifndef MOVIETREE_H
+#define MOVIETREE_H
+#include <iostream>
+
+struct QuoteNode{
+    std::string quote;
+    std::string author;
+    QuoteNode *parent;
+    QuoteNode *left;
+    QuoteNode *right;
+
+    QuoteNode(){};
+
+    QuoteNode(std::string in_quote, std::string in_author)
+    {
+        quote = in_quote;
+        author = in_author;
+        parent = NULL;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+class QuoteTree
+{
+
+    public:
+        QuoteTree();
+        ~QuoteTree();
+        int countQuoteNodes();
+        void addQuoteNode(std::string quote, std::string author);
+        void findQuote(std::string quote, std::string author);
+
+    protected:
+
+    private:
+        void DeleteAll(QuoteNode * node); //use this for the post-order traversal deletion of the tree
+        QuoteNode* search(std::string quote);
+        QuoteNode* treeMinimum(QuoteNode *node);
+        QuoteNode *root;
+        int quoteCount;
+};
+
+#endif // MOVIETREE_H
